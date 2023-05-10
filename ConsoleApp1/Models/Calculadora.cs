a058e33f15614d5795bc69950c4b9dd247a51b63
KaioDD <kkaiodantas6@gmail.com>
2023-05-10 11:29:06 -0300

    Adicionar arquivos de projeto.

---
+++ ConsoleApp1/Models/Calculadora.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1.Models
+{
+    internal class Calculadora
+    {
+      public void Somar(int x, int y) 
+        {
+            Console.WriteLine($"{x} + {y} = {x + y}");
+        }
+
+      public void Subtrair(int x, int y) 
+        {
+            Console.WriteLine($"{x} - {y} = {x - y}");
+        }
+        public void Multiplicar(int x, int y) 
+            {
+            Console.WriteLine($"{x} * {y} = {x * y}");
+            }
+        public void Dividir(int x, int y)
+        {
+            Console.WriteLine($"{x} / {y} = {x / y}");
+        }
+        public void Potencia(int x, int y)
+        {
+            double pot = Math.Pow(x, y);
+            Console.WriteLine($"{x}^{y} = {pot}");
+        }
+
+        public void Seno(double angulo)
+        {
+            double radiano = angulo * Math.PI / 180;
+            double seno = Math.Sin(radiano);
+            Console.WriteLine($"Seno de {angulo} = {Math.Round(seno, 4)}");
+        }
+
+        public void coseno(double angulo)
+        {
+            double radiano = angulo * Math.PI / 180;
+            double coseno = Math.Cos(radiano);
+            Console.WriteLine($"Coseno de {angulo} = {Math.Round(coseno, 4)}");
+        }
+
+        public void tangente(double angulo)
+        {
+            double radiano = angulo * Math.PI / 180;
+            double tangente = Math.Tan(radiano);
+            Console.WriteLine($"tangente de {angulo} = {Math.Round(tangente, 4)}");
+        }
+        public void raizQuadrada(double x)
+        {
+            double raiz = Math.Sqrt(x);
+            Console.WriteLine($"Raiz quadrada de {x} = {raiz}");
+        }
+
+    }
+}
---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,19 @@
+﻿using ConsoleApp1.Models;
+
+Calculadora calc = new Calculadora();
+
+calc.Somar(10, 20);
+calc.Subtrair(10, 20);
+calc.Multiplicar(10, 20);
+calc.Dividir(100, 20);
+calc.Seno(30);
+calc.tangente(30);
+calc.coseno(30);
+calc.raizQuadrada(20);
+
+
+
+
+
+
+
